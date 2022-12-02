241a3fad14be7875d9b6c70b7c8eafbb820e65f2
FractureDVL <joseandrescram1005@gmail.com>
2022-12-02 09:40:39 -0500

    fixed vue show data

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return { 
+        id: '0',
+        llegada : '1',
+        tiempo : '3',
+        prioridad: '3',
+    };
+  },
+});
+app.mount("#app");
