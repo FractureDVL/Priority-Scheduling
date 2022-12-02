e615d003d6d8f271ce72cb41e548d895f1d85cff
FractureDVL <joseandrescram1005@gmail.com>
2022-12-01 23:39:09 -0500

    Created project in vue

---
+++ Scheduling/Process.js
@@ -0,0 +1,16 @@
+const procesos = [];
+class Process {
+    constructor(id, llegada, tiempo, prioridad) {
+        this.id = id;
+        this.llegada = llegada;
+        this.tiempo = tiempo;
+        this.prioridad = prioridad;
+    }
+}
+function addProcess(proceso){
+    procesos.push(proceso);
+}
+
+//const P1 = new Proceso("P1", 0 , 20 , 1);
+
+export {addProcess, procesos};
